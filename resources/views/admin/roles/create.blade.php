7b9dd2ce542c4b1f270b15c480c637d68e37557b
rowda <rowdaabdella1@gmail.com>
2023-09-26 12:29:43 +0300

    Refactor UI for role and permission create views

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -1,16 +1,18 @@
 <x-admin-layout>
-    <div class="  py-10 px-2 w-full">
-        <h1 class=" pb-4 font-bold">CREATE NEW ROLES</h1>
-        <div class=" rounded-sm bg-white">
-            <div class="flex justify-between items-center rounded-t-md text-white bg-gray-400  h-14 p-3 overflow-hidden">
+    <div class="py-12 w-full">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-2">
+                <h1 class=" p-2 font-extrabold">CREATE NEW ROLES</h1>
+
+
+                {{-- <div class="  py-10 px-2 w-full"> --}}
+
+                {{-- <div class=" rounded-sm bg-white"> --}}
+                {{-- <div class="flex justify-between items-center rounded-t-md text-white bg-gray-400  h-14 p-3 overflow-hidden">
                 <h3>Add mew Role</h3>
-                <a class=" flex bg-red-400 py-1 items-center gap-1 px-2 rounded-md text-black  "
-                    href="{{ route('admin.roles.index') }}">
-                    <i class="fa-regular fa-eye text-white font-xl"></i>
-                    See All Roles
-                </a>
-            </div>
-            {{-- <form method="POST" action="{{ route('admin.roles.store') }}">
+
+            </div> --}}
+                {{-- <form method="POST" action="{{ route('admin.roles.store') }}">
                 @csrf
                 <div class="p-6">
                     <input type="text" name="name" class="outline outline-blue-300 outline-1 w-full rounded-md"
@@ -47,50 +49,48 @@
             Save Role
         </button>
         </form> --}}
-        <form method="POST" action="{{ route('admin.roles.store') }}">
-            @csrf
-            <div class="p-6">
-                <input
-                    type="text"
-                    name="name"
-                    id="roleName"
-                    class="outline outline-blue-300 outline-1 w-full rounded-md"
-                    placeholder="Role Name"
-                >
-        
-                <div>
-                    <h3 class="py-4 font-semibold">Assign Permissions</h3>
-                    <div class="flex flex-wrap -mx-2">
-                        @foreach ($permissions as $permission)
-                            <div class="w-1/4 px-2 py-1 font-semibold">
-                                <input
-                                    class="rounded-md shadow-lg"
-                                    type="checkbox"
-                                    name="permissions[]"
-                                    id="permission{{ $permission->id }}"
-                                    value="{{ $permission->id }}"
-                                >
-                                <label for="permission{{ $permission->id }}">{{ $permission->name }}</label>
+
+                <form method="POST" action="{{ route('admin.roles.store') }}">
+                    @csrf
+                    <div class="flex flex-col p-2 bg-slate-100">
+                        <div class="p-6">
+                            <input type="text" name="name" id="roleName"
+                                class="outline outline-blue-300 outline-1 w-full rounded-md" placeholder="Role Name">
+
+                            <div>
+                                <h3 class="py-4 font-extrabold">ASSIGN PERMISSIONS</h3>
+                                <div class="flex flex-wrap -mx-2 font-bold">
+                                    @foreach ($permissions as $permission)
+                                        <div class="w-1/4 px-2 py-1 ">
+                                            <input class="rounded-md shadow-lg text-sm" type="checkbox"
+                                                name="permissions[]" id="permission{{ $permission->id }}"
+                                                value="{{ $permission->id }}">
+                                            <label for="permission{{ $permission->id }}">{{ $permission->name }}</label>
+                                        </div>
+                                    @endforeach
+                                </div>
                             </div>
-                        @endforeach
+                        </div>
+                        <div class="flex justify-end gap-2 mr-10">
+                            <button type="submit"
+                                class="bg-blue-500 my-4 px-6 py-2 rounded-md text-white flex items-center gap-1">
+                                {{-- <i class="fa-regular fa-floppy-disk font-bold w-4"></i> --}}
+                                Save Role
+                            </button>
+
+                            <a class=" bg-red-300 my-4 px-6 py-2 rounded-md text-white flex items-center gap-1  "
+                                href="{{ route('admin.roles.index') }}">
+                                Cancel
+                            </a>
+                        </div>
                     </div>
-                </div>
+
+                </form>
+
             </div>
-            <button
-                type="submit"
-                class="bg-gray-500 my-4 px-6 py-2 rounded-md text-white flex items-center gap-1"
-            >
-                <i class="fa-regular fa-floppy-disk font-bold w-4"></i>
-                Save Role
-            </button>
-        </form>
-        
+
+        </div>
+    </div>
     </div>
 
-    </div>
-
-
-
-    {{-- </div>
-    </div> --}}
 </x-admin-layout>
