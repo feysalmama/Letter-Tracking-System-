dbed36e2db4fce186fe23fc3cccc10d7cca876ed
Oum Ja <oum.burs9mry@gmail.com>
2023-10-18 11:25:16 +0300

    correcting login page layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -149,9 +149,11 @@
                         <li class="relative">
                             <button class="align-middle rounded-full focus:shadow-outline-purple focus:outline-none"
                                 @click="toggleProfileMenu" aria-label="Account" aria-haspopup="true">
+                                @if(Auth::user())
                                 <img class="object-cover w-8 h-8 rounded-full"
-                                    src="{{Auth::user()->image}}"
+                                    src=" {{ asset('user/' . Auth::user()->image)}}"
                                     alt="" aria-hidden="true" />
+                                @endif
                 
                             </button>
                             <div x-show="isProfileMenuOpen">
@@ -161,6 +163,7 @@
                                     class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:border-gray-700 dark:text-gray-300 dark:bg-gray-700"
                                     aria-label="submenu">
                                     <li class="flex">
+                                        @if(Auth::user())
                                         <a class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
                                             href="{{ route('admin.users.edit', Auth()->user()->id) }}">
                                             <svg class="w-4 h-4 mr-3" aria-hidden="true" fill="none"
@@ -172,6 +175,7 @@
                                             </svg>
                                             <span>Profile</span>
                                         </a>
+                                        @endif
                                     </li>
                                     <li class="flex">
                                         <a class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
