7708e7ce6d155948e682f47da7fe5340c22265aa
oum burs <oum.burs9mry@gmail.com>
2023-12-11 23:21:05 +0300

    navigation fixed

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -23,7 +23,7 @@
 
         <nav class="bg-white border-gray-200 dark:bg-gray-900 fixed top-0 w-full z-50">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="{{route('dashboard')}}" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Letters</span>
                 </a>
